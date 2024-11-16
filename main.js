0d288a2dbd9d5620627fc1c3e142640305382f1d
Ilgizik <ilishev.i@yandex.ru>
2024-11-16 18:34:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Вытащили кнопку
+const btn = document.querySelector('.article__button');
+
+// Вытащили картинку стрелочек рядом с кнопкой
+const icon = document.querySelector('.article__button--img'); 
+
+// Переменная для скрытых элементов, меняется в зависимости от ширины экрана
+let elements = document.querySelectorAll('.article__list--item:nth-child(n + 7)');
+
+// обработчик событий для кнопки
+btn.addEventListener('click', function () {
+    console.log(elements);
+// цикл, проходящий по всем скрытым элементам, и меняющий всем им классы с помощью classList.toggle
+    for (let i = 0; i < elements.length; i++) {    
+    elements[i].classList.toggle('show');
+    }
+// Условия, если текст кнопки один, поменять его на другой, и все это в обработчике событий
+    if (btn.textContent === 'Показать все') {
+        btn.textContent = 'Скрыть';
+    } else {
+        btn.textContent = 'Показать все';
+    }
+    // Ротате
+    const currentRotation = icon.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+    icon.style.transform = currentRotation;
+});
+
+// свайпер
+new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+      }
+         
+})
+
+
